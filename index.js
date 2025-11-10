b14d7d5b528f05f23bc972f8dab9933b36cc79e2
Abrarul Rhythm <[....email>
2025-11-10 22:42:10 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +9,36 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrthjko.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Plate Share server is running...');
 })
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('plateShare_db');
+        const usersCollection = db.collection('users');
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log(`Plate Share server is running on port: ${port}`);
