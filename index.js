a5d7868f3058e6a9ecbb67bb772c2199fd5b1f5a
Abrarul Rhythm <[....email>
2025-11-11 22:37:14 +0600

    added get api for single food

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -96,6 +96,14 @@ async function run() {
             const query = { foodStatus: 'Available' };
             const cursor = foodsCollection.find(query).sort({ foodQuantity: -1 }).limit(6);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // Get API for single food
+        app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await foodsCollection.findOne(query);
             res.send(result);
         })
 
